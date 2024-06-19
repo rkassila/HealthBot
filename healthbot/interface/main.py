a90d8d639d0ed1a7287dfce94ecf9105562f7e6d
Reda KASSI LAHLOU <rkassila@gmail.com>
2024-06-19 21:40:20 +0900

    Modified main

--- healthbot/interface/main.py
+++ healthbot/interface/main.py
@@ -52,6 +52,8 @@ if "symptoms" in st.session_state:
         cols = st.columns(num_columns)
         for col, symptom in zip(cols, symptoms[i:i + num_columns]):
             scale_key = f"{symptom}_scale"
+            # Display symptom in bold
+            #col.markdown(f"<b>Severity for {symptom}:</b>", unsafe_allow_html=True)
             # Create radio buttons with a unique key
             col.radio(
                 f"Severity for {symptom}",
