cc7dd3721efea6bea68a47dc6000002984ccce57
Reda KASSI LAHLOU <rkassila@gmail.com>
2024-06-25 15:10:59 +0900

    New symptoms function implemented

--- healthbot/interface/logic.py
+++ healthbot/interface/logic.py
@@ -5,11 +5,16 @@ import re
 # List of stop words to remove
 stop_words = {'the', 'is', 'at', 'which', 'on', 'and', 'a', 'an', 'of', 'to', 'in', 'with', 'for'}
 
-# Creating mapping diseases df
-relative_path = os.path.join("databases", "diseases", "Symptom-severity.csv")
-symptoms_df = pd.read_csv(relative_path)
+# Importing symptoms df
+relative_path_symptoms = os.path.join("databases", "diseases", "Symptom-severity.csv")
+symptoms_df = pd.read_csv(relative_path_symptoms)
 symptoms = symptoms_df['Symptom']
 mapping = {symptom: symptom.split('_') for symptom in symptoms}
+
+# Importing diseases df
+relative_path_diseases = os.path.join("databases", "diseases", "dataset.csv")
+diseases_df = pd.read_csv(relative_path_diseases)
+
 
 def match_symptom(symptom, test_list):
     words = mapping.get(symptom, [])
@@ -23,3 +28,21 @@ def response_generator_symptoms(prompt):
     matched_symptoms = [symptom for symptom in symptoms if match_symptom(symptom, filtered_words)]
 
     return matched_symptoms
+
+def check_symptoms(row, symptoms):
+    row_symptoms = row.dropna().str.strip().tolist()
+    return all(symptom in row_symptoms for symptom in symptoms)
+
+def response_new_symptoms(input_symptoms):
+    matched_diseases = diseases_df[diseases_df.apply(check_symptoms, symptoms=input_symptoms, axis=1)]
+    symptom_columns = [f'Symptom_{i}' for i in range(1, 18)]
+    all_symptoms = pd.Series(dtype='str')
+
+    for col in symptom_columns:
+        all_symptoms = pd.concat([all_symptoms, matched_diseases[col]])
+
+    unique_symptoms = all_symptoms.dropna().str.strip().unique()
+    unique_symptoms_list = unique_symptoms.tolist()
+    unique_symptoms_not_in_input = list(set(unique_symptoms_list) - set(input_symptoms))
+
+    return unique_symptoms_not_in_input
--- healthbot/interface/main_backup.py
+++ healthbot/interface/main_backup.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from logic import response_generator_symptoms  # To be replaced by symptoms detector
+from logic import response_generator_symptoms, response_new_symptoms  # To be replaced by symptoms detector
 
 MAX_COL = 5
 
@@ -13,7 +13,7 @@ with col2:
     # Reload button to reset the app
     if st.button("Reload app"):
         st.session_state.clear()
-        st.experimental_rerun()
+        st.rerun()
 
 # Initialize chat history and phase state
 if "messages" not in st.session_state:
@@ -62,7 +62,7 @@ if st.session_state.phase == 1:
 
         if st.button("Confirm Symptoms"):
             st.session_state.confirm_clicked = True
-            st.experimental_rerun()
+            st.rerun()
 
     if st.session_state.confirm_clicked:
         cols = st.columns(len(st.session_state.symptoms))
@@ -79,7 +79,7 @@ if st.session_state.phase == 1:
         st.session_state.messages.append({"role": "assistant", "content": "Please describe any current treatments and relevant health history"})
         st.session_state.phase = 2
         st.session_state.confirm_clicked = False
-        st.experimental_rerun()
+        st.rerun()
 
 # Phase 2: Describe treatments and history
 if st.session_state.phase == 2:
@@ -99,54 +99,59 @@ if st.session_state.phase == 2:
 
         # Move to Phase 3
         st.session_state.phase = 3
-        st.experimental_rerun()
+        st.rerun()
 
 # Phase 3: Other symptoms displayed for confirmation
 if st.session_state.phase == 3:
 
     # Send to API for diseases prediction (TO DO)
-    # TO CODE
-    new_symptoms_list = ['symptom_1', 'symptom_2'] # to be replaced by API call
+    new_symptoms_list = response_new_symptoms(st.session_state.symptoms)
     st.session_state.new_scaled_symptoms = {}
 
     if "new_symptoms" not in st.session_state:
         st.session_state.new_symptoms = new_symptoms_list
 
-    st.markdown("### Have you been experiencing the following symptoms:")
-    num_columns = min(MAX_COL, len(st.session_state.new_symptoms))
-    for i in range(0, len(st.session_state.new_symptoms), num_columns):
-        cols = st.columns(num_columns)
-        for col, symptom in zip(cols, st.session_state.new_symptoms[i:i + num_columns]):
-            scale_key = f"new_{symptom}_scale"
-            if scale_key not in st.session_state:
-                st.session_state[scale_key] = 0
-            st.session_state.new_scaled_symptoms[symptom] = col.radio(
-                f"Severity for {symptom}",
-                options=list(range(5)),
-                index=st.session_state[scale_key],
-                key=scale_key,
-            )
+    if not st.session_state.new_symptoms:
+        st.markdown("### Click confirm please")
+    else:
+        st.markdown("### Have you been experiencing the following symptoms:")
+        num_columns = min(MAX_COL, len(st.session_state.new_symptoms))
+        for i in range(0, len(st.session_state.new_symptoms), num_columns):
+            cols = st.columns(num_columns)
+            for col, symptom in zip(cols, st.session_state.new_symptoms[i:i + num_columns]):
+                scale_key = f"new_{symptom}_scale"
+                if scale_key not in st.session_state:
+                    st.session_state[scale_key] = 0
+                st.session_state.new_scaled_symptoms[symptom] = col.radio(
+                    f"Severity for {symptom}",
+                    options=list(range(5)),
+                    index=st.session_state[scale_key],
+                    key=scale_key,
+                )
 
-    if st.button("Confirm New Symptoms"):
+    if st.button("Confirm"):
         st.session_state.confirm_clicked = True
-        st.experimental_rerun()
+        st.rerun()
 
     if st.session_state.confirm_clicked:
-        cols = st.columns(len(st.session_state.new_symptoms))
         new_symptom_severity_message = ""
-        for col, symptom in zip(cols, st.session_state.new_symptoms):
-            severity = st.session_state[f"new_{symptom}_scale"]
-            if severity != 0:
-                new_symptom_severity_message += f"**{symptom}:** {severity}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+        if st.session_state.new_symptoms:
+            cols = st.columns(len(st.session_state.new_symptoms))
+            for col, symptom in zip(cols, st.session_state.new_symptoms):
+                severity = st.session_state[f"new_{symptom}_scale"]
+                if severity != 0:
+                    new_symptom_severity_message += f"**{symptom}:** {severity}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
 
-        st.session_state.messages.append({"role": "assistant", "content": new_symptom_severity_message})
+        if new_symptom_severity_message:
+            st.session_state.messages.append({"role": "assistant", "content": new_symptom_severity_message})
 
-        #Send to API for corrected diseases prediction
+        # Send to API for corrected diseases prediction
         # Generate file (with every symptom and best x predictions)
         # File displayed and thanks
         st.session_state.messages.append({"role": "assistant", "content": "Thank you for your input!"})
         st.session_state.phase = 4
-        st.experimental_rerun()
+        st.rerun()
+
 
 # Send to API for corrected diseases prediction
 # TO CODE
